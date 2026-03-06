47a84ead3cbdbd19b5fbaa49490adc7443471167
Amirul Islam <amirul.csejust@gmail.com>
2026-03-06 21:21:28 +0600

    server update

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('STRAPI_URL', 'http://localhost:1337'),
+  host: env('HOST', '0.0.0.0'),           // allow external connections
+  port: env.int('PORT', 1337),            // server port
+  url: env('PUBLIC_URL', 'https://debhatasamityadmin.bmhbd.org'), // <-- set your domain
   app: {
     keys: env.array('APP_KEYS'),
   },
